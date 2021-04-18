d871e3efd56b4cbce7430e193817d3e93cc4895d
sitcode19 <pnch_mai@hotmail.com>
2021-04-18 10:24:11 +0700

    change function c2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <title>Introduction to Git and Github by DevAhoy</title>
+    <title></title>
   </head>
   <body>
     <h1>change function c</h1>
